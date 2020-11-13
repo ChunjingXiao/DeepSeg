fc158cb8544f606d125537875086604605e9c4d2
Chunjing Xiao <chunjingxiao@gmail.com>
2020-11-13 21:24:39 +0800

    Delete 3train_4_200_30_3_Mat2npy_Weight_Segm.bat

--- 12CnnSegmentCode/3train_4_200_30_3_Mat2npy_Weight_Segm.bat
+++
@@ -1,9 +0,0 @@
-
-
-
-python 3train_4_200_30_3_Mat2npy_Weight_Segm.py --trainCsi segmentBaseTrainCsi.mat --trainLab segmentBaseTrainLab.mat --lr 0.0005
-
-::python 3train_4_200_30_3_Mat2npy_Weight_Segm.py --trainCsi segmentBaseTrainCsi.mat --trainLab segmentBaseTrainLab.mat --lr 0.0005 --wtrain 1
-
-pause
-
